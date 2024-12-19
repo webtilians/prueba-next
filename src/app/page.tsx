7db2ea8ed793c310e8424b5d01c58046eb7a3b8f
webtilians <enrikelr@hotmail.com>
2024-12-19 01:19:25 +0100

    server sesion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-'use client';
-import Image from "next/image";
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push('/login');
-  }, [router]);
+import { redirect } from 'next/navigation';
 
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* Puedes mostrar un spinner de carga mientras redirige */}
-    </div>
-  );
+export default function Home() {
+  // Redirigir al login
+  redirect('/login');
+  
+  return null; // No se renderiza nada ya que se redirige al login
 }
